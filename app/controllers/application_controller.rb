68b660088382944940a8b9b9fb2744e6f43318ac
Definence <dp210592ybo@gmail.com>
2020-04-27 20:14:46 +0300

    Add header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :define_collections
 
   def define_collections
-    @collections ||= ShopifyAPI::SmartCollection.find(:all).map(&:handle)
+    @collections ||= ShopifyAPI::SmartCollection.find(:all).map do |collection|
+      tags = []
+      collection.products.map do |product|
+        product_tags = product.tags.split(', ')
+        tags.concat product_tags
+      end
+
+      { title: collection.title, tags: tags.uniq.reject { |c| c.empty? } }
+    end
   end
 end
--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,6 @@
 class CollectionsController < ApplicationController
   def show
-    @smart_collection = ShopifyAPI::SmartCollection.where(handle: params[:collection_type])&.first
+    @smart_collection = ShopifyAPI::SmartCollection.where(title: params[:type])&.first
     return head 404 if @smart_collection.blank?
 
     @products = @smart_collection.products
